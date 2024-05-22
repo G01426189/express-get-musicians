49d78b1d9456f6280187f4bddbc02007bb92a97d
G01426189 <jacobpaulwhitehead@gmail.com>
2024-05-22 14:54:39 +0100

    .

--- src/app.js
+++ src/app.js
@@ -6,13 +6,19 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
-app.get("/musicians", async (req, res) => {
-    const musician = await Musician.findAll();
-    res.json(musician);
-})
-
-
-
+app.get('/musicians/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const musician = await Musician.findByPk(id);
+        if (musician) {
+            res.json(musician);
+        } else {
+            res.status(404).json({ error: 'Musician not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
 
 
 
